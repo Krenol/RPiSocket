a4e51905caea2119b882582062a5ac20309a2844
Krenol <lauritzgroeger@gmail.com>
2020-05-17 19:58:37 +0200

    small fixes

--- rpisocket/test/src/test.cpp
+++ rpisocket/test/src/test.cpp
@@ -5,30 +5,12 @@
 #include <functional>
 #include <string>
 #include <cstdlib>
-#include <map>
-#include <stdlib.h>
 
-static std::map<int, std::string> statusMap = {{200, "OK"}, {201, "Created"}, {202, "Accepted"}, {400, "Bad Request"}};
-
-std::string createRestMsg(int statusCode, std::string contentType, std::string charset, std::string msg) {
-    std::time_t cur_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    auto time_str = std::to_string(cur_time);
-    auto statusMsg = statusMap.count(statusCode) ? statusMap.at(statusCode) : "";
-    auto response = "HTTP/1.1 " + std::to_string(statusCode) + " " + statusMsg + "\n";
-    response += "Server: RPi\nContent-Type: " + contentType + "; charset=" + charset + "\n";
-    response += "Date: " + time_str + "\nKeep-Alive: timeout=5, max=1000\nConnection: Keep-Alive\nContent-Length: ";
-    response += std::to_string(msg.length()) + "\n\n" + msg; 
-    return response;
-}
-
-void test(const std::string& val) {
-
-}
 
 int main() {
     int port;
     try{
-        auto env_var = std::getenv("SOCKET_PORT");
+        auto env_var = getenv("SOCKET_PORT");
         if(env_var){
             std::cout << "env_var: " << env_var << std::endl;
             port = std::stoi(env_var); 
@@ -40,8 +22,6 @@ int main() {
     } catch(...){
         port = 8888;
     }
-
-    
     
     int number = 1;
     //rpisocket::BTServer server;
