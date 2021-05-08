cd9ba55d21742ef61f6223c34029489c0f3984dc
Krenol <lauritzgroeger@gmail.com>
2021-05-08 19:03:45 +0200

    ..

--- rpisocket/test/src/test.cpp
+++ rpisocket/test/src/test.cpp
@@ -6,34 +6,47 @@
 #include <string>
 #include <cstdlib>
 
-
-int main() {
-    int port = 8888;
+int main()
+{
+    int port = 8889;
     //rpisocket::BTServer server;
     rpisocket::WiFiServer server(port);
     std::cout << "created socket on port " << port << " with ip address " << server.getServerAddress() << std::endl;
-    srand (static_cast <unsigned> (time(0)));
+    srand(static_cast<unsigned>(time(0)));
     std::string msg, buf = "", delimiter_ = "\n";
-    int msg_size_=150;
-    
-    while(true){
+    int msg_size_ = 150;
+
+    while (true)
+    {
         std::cout << "waiting for connection..." << std::endl;
         server.connect();
         std::cout << "success!" << std::endl;
         std::size_t pos;
-        while(server.hasConnection()){
-             try{
+        while (server.hasConnection())
+        {
+            try
+            {
                 //read incoming msg
-                while(1){
-                    server.readBytes(msg);
-                    buf += msg;
-                    while ((pos = buf.find(delimiter_)) != std::string::npos) {
-                        msg = buf.substr(0, pos);
-                        std::cout << "read msg: " << msg << std::endl << std::endl;
-                        buf.erase(0, pos + delimiter_.length());
+                while (1)
+                {
+                    if (server.hasData())
+                    {
+                        server.readBytes(msg);
+                        buf += msg;
+                        while ((pos = buf.find(delimiter_)) != std::string::npos)
+                        {
+                            msg = buf.substr(0, pos);
+                            std::cout << "read msg: " << msg << std::endl
+                                      << std::endl;
+                            buf.erase(0, pos + delimiter_.length());
+                        }
+                    } else {
+                        std::cout << "no data received--\n";
                     }
                 }
-            } catch(...) {
+            }
+            catch (...)
+            {
                 // on error stop thread
                 break;
             }
